ab4a13338c012ec14277af2a8cf5fe36714619fe
Vincentlai <qla64@sfu.ca>
2015-10-17 14:18:48 -0700

    the sign up , login , logout page done , working on the styling

--- CarPoolApp/app/helpers/application_helper.rb
+++ CarPoolApp/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #return title of the page
+  def full_title(page_title = '')
+    base_title = "Car Pool App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
--- CarPoolApp/config/routes.rb
+++ CarPoolApp/config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
 
+  root 'sessions#new'
+
+  get 'signup' => 'users#new'
+
+  get 'about' => 'pages#about'
+
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
+
+
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
